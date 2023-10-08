f4ffc3d91c2e1a253041fca6c866a3879b269c32
DiyaGariba <hamdiya19tobe@gmail.com>
2023-10-08 07:12:49 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,48 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
-* main - A program that prints combination of all two digit numbers 0-99.
- *
- * Return: Always 0 (Success)
+ * main - main block
+ * Description: Print all possible combinations of two two-digit numbers.
+ * Numbers should range from 0 to 99.
+ * The two numbers should be separated by a space.
+ * All numbers should be printed with two digits. 1 should be printed as 01.
+ * Combination of numbers must be separated by a comma followed by a space.
+ * Combinations of numbers should be printed in ascending order.
+ * `00 01` and `01 00` are considered as the same combination.
+ * You can only use `putchar` to print to console.
+ * You can only use `putchar` up to 8 times.
+ * You are not allowed to use any variable of type `char`.
+ * Return: 0
  */
 
 int main(void)
 {
-	int fTens;
-	int fOnes;
-	int sTens;
-	int sOnes;
-	int num, num2;
+	int x, y;
 
-	for (num = 0 ; num <= 99 ; num++)
+	for (x = 0; x < 100; x++)
 	{
-	fTens = num / 10;
-	fOnes = num % 10;
-
-	for (num2 = 0 ; num2 <= 9 ; num2++)
-	{
-		sTens = num2 / 10;
-		sOnes = num2 % 10;
-
-		if (fTens < sTens || (fTens == sTens && fOnes < sOnes))
+		for (y = 0; y < 100; y++)
 		{
-			putchar(fTens + '0');
-			putchar(fOnes + '0');
-			putchar(' ');
-			putchar(sTens + '0');
-			putchar(sOnes + '0');
-
-			if (!(fTens == 9 && fOnes == 8))
+			if (x < y)
 			{
-				putchar(',');
+				putchar((x / 10) + 48);
+				putchar((x % 10) + 48);
 				putchar(' ');
+				putchar((y / 10) + 48);
+				putchar((y % 10) + 48);
+				if (x != 98 || y != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
-
 		}
 	}
-	}
 	putchar('\n');
-
-
 	return (0);
 }
